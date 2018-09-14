944f0adac156ca8d36ec6e5fe7b56330a4512c00
Fernan de Dios <info@mediagramstudios.com>
2018-09-14 14:44:19 +0800

    app test modifications

---
+++ src/tests/app/App.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import App from '../../app/App';
+import ReactTestRenderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store'
+
+const initialState = {}; 
+const mockStore = configureStore();
+let store;
+
+describe('<App />', () => {
+  beforeEach(() => {
+    store = mockStore(initialState)
+   });
+
+  it('renders without crashing', () => {
+    shallow(<App store={store} />);
+  });
+});
