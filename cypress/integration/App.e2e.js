56f3e4d1ea87904bbc05c59cd5e19df1c2a42984
Fernan de Dios <info@mediagramstudios.com>
2018-09-18 09:55:59 +0800

    added initial cypress e2e tests

---
+++ cypress/integration/App.e2e.js
@@ -0,0 +1,21 @@
+describe('<App /> E2E', () => {
+  it('renders navbar', () => {
+    cy.visit('/');
+    cy.get('nav').should('to.exist');
+  });
+
+  it('renders footer', () => {
+    cy.visit('/');
+    cy.get('footer').should('to.exist');
+  });
+
+  it('renders search form', () => {
+    cy.visit('/');
+    cy.get('formn').should('to.exist');
+  });
+
+  it('renders search input field', () => {
+    cy.visit('/');
+    cy.get('#field_eth').should('to.exist');
+  });
+});
